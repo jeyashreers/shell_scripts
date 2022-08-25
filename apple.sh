c18740c64b7a1e60aa233e3ad58d99507a880c7a
EC2 Default User <ec2-user@ip-172-31-4-74.us-west-2.compute.internal>
2022-08-25 07:29:49 +0000

    apple.sh

--- apple.sh
+++ apple.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "enter the name of fruit"
 read a
-test=`echo $a | grep [A-Z]`
+t=`echo $a | grep [A-Z]`
 if [ "$?" = "0" ]; then
-               b=`echo $test | tr '[A-Z]' '[a-z]'`
-               if [ $b == "apple" ]; then
+               b=`echo $t | tr '[A-Z]' '[a-z]'`
+               if [ "$b" == "apple" ]; then
                                echo "the entered fruit is correct"
                        else
                                        echo "the entered fruit is not correct.The fruit entered is $b"
